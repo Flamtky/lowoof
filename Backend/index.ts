08ab0c84b90cd8cb9dc9a9333f69737d3f9ad00a
Luca-Miguel Humke <luca.humke@web.de>
2021-12-02 09:36:50 +0100

    Added Api deklaration

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import mysql from 'mysql';
  
 const app: express.Application = express();
 const port: number = 3000;
 
+const mySqlHost: string = 'db5005941567.hosting-data.io';
+const mySqlUser: string = 'dbu1141556';
+const mySqlPassword: string = 'C8Gh-aVYx=Bcq6pc7-WZ';
+const mySqlDatabase: string = 'dbs4979312';
+
+function getConnection() {
+    return mysql.createConnection({
+        host: mySqlHost,
+        user: mySqlUser,
+        password: mySqlPassword,
+        database: mySqlDatabase
+    });
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.json({message: 'Welcome to lowoof-API!'});
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
---
+++ Backend/lowoof-api.ts
@@ -0,0 +1,87 @@
+function isSessionIDValid(sessionID: string): boolean {
+    return false;
+}
+
+function destroySession(sessionID: string): void {
+    return;
+}
+
+function isUsernameValid(username: string): boolean {
+    return false;
+}
+
+function isPasswordValid(password: string): boolean {
+    return false;
+}
+
+function createNewUser(user: any): void {
+    return;
+}
+
+function getProfileData(userId: number): any{
+    return;
+}
+
+function updateProfile(newProfile: any): void {
+    return;
+}
+
+function deleteUser(userId: number): void {
+    return;
+}
+
+function createPetProfile(petProfile: any): void {
+    return;
+}
+
+function getFriends(userId: number): any {
+    return;
+}
+
+function addFriend(userId: number, friendId: number): void {
+    return;
+}
+
+function removeFriend(userId: number, friendId: number): void {
+    return;
+}
+
+function addReport(userId: number, reason: string): void {
+    return;
+}
+
+function likePet(linkingPetId: number, petId: number): void {
+    return;
+}
+
+function dislikePet(dislinkingPetId: number, petId: number): void {
+    return;
+}
+
+function getChats(userId: number): any {
+    return;
+}
+
+function removeChat(deletingUserId: number, userId: number): void {
+    return;
+}
+
+function getPets(userId: number): any {
+    return;
+}
+
+function getMatches(userId: number): any {
+    return;
+}
+
+function getMessages(userId: number, toUserId:number): any {
+    return;
+}
+
+function sendMessage(userId: number, toUserId: number, message: string): void {
+    return;
+}
+
+function pauseProfile(userId: number): void {
+    return;
+}

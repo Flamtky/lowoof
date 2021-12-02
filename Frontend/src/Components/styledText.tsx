9d76d00326ded14f0bec5cebce447c8b93f0f77e
Flamtky <n.schumann@rz-online.de>
2021-12-02 07:08:21 +0100

    changed font

--- Frontend/src/Components/styledText.tsx
+++ Frontend/src/Components/styledText.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Text } from 'react-native';
 
 export function TextBlock(props: Text["props"]) {
-    return <Text{...props} style={[props.style, {fontFamily:"normal"}]}/>;
+    return <Text{...props} style={[props.style, {fontFamily:"sans-serif"}]}/>;
 }

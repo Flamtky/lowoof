ed0b8e6cb6f45c8e9a7ff72900d2355f70de3f07
Flamtky <n.schumann@rz-online.de>
2021-12-02 08:07:11 +0100

    added new colors

--- Frontend/src/Constants/colors.tsx
+++ Frontend/src/Constants/colors.tsx
@@ -1,3 +1,7 @@
 export const MAINCOLOR = '#cae0f8';
 export const BACKGROUNDCOLOR = '#9fc8f9';
 export const TITLECOLOR = '#555';
+export const BLACK = '#000';
+export const WHITE = '#fff';
+export const GRAY = "#333";
+export const BLUE = "#0F70E6";

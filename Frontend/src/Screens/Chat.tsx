24187d2c68f7c9b27f99b47945481e9585a08c63
Flamtky <n.schumann@rz-online.de>
2021-12-04 23:42:27 +0100

    added chat | message history missing

--- Frontend/src/Screens/Chat.tsx
+++ Frontend/src/Screens/Chat.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, useWindowDimensions, TextInput } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import OwnButton from '../Components/ownButton';
+import SearchBar from '../Components/searchbar';
 import { BACKGROUNDCOLOR, MAINCOLOR } from '../Constants/colors';
 
 export function Chat() {
@@ -7,8 +10,17 @@ export function Chat() {
     const isLargeScreen = dimensions.width >= 768;
     return (
         <View style={{ backgroundColor: BACKGROUNDCOLOR, height: "100%" }}>
-            <View style={[styles.container, isLargeScreen ? { width: '43%', left: "28%" } : { width: "100%" }]}>
-                <Text>Chat mit XY</Text>
+            <View style={[styles.container, isLargeScreen ? { width: '43%', left: "28%" } : null]}>
+                <ScrollView keyboardDismissMode='on-drag' style={{ height: "100%", width: "100%" }}>
+                    {/*Messages*/}
+                </ScrollView>
+                <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
+                    <SearchBar
+                        placeholder="Nachricht...."
+                        style={styles.input}
+                    />
+                    <OwnButton title="➤" style={{ width: 40, padding: 0, minWidth: 0, borderRadius: 0, bottom: -0.3 }} />
+                </View>
             </View>
         </View>
     );
@@ -20,5 +32,12 @@ const styles = StyleSheet.create({
         backgroundColor: MAINCOLOR,
         alignItems: 'center',
         justifyContent: 'center',
+        height: "100%"
+    },
+    input: {
+        height: 42,
+        width: "90%",
+        alignSelf: 'flex-end',
+        borderRadius: 0,
     },
 });

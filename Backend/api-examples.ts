b641623ff2e52a899f3551e06ec2e219dcf68424
Luca-Miguel Humke <luca.humke@web.de>
2021-12-04 13:32:43 +0100

    Added API exampes

---
+++ Backend/api-examples.ts
@@ -0,0 +1,42 @@
+import { Api } from "./lowoof-api";
+const api: Api = new Api();
+
+async function start(){
+    await api.getAuthTokenfromServer("username", "password"); //Needs to be called once to get the token
+    createUser();
+    profileData(1);
+    validUsername("username");
+}
+
+//This creates a new user, you need to check if the username is valid 
+//by urself
+async function createUser() {
+    var user:any = {
+        USERNAME: 'nutzername',
+        PASSWORD: 'sicheresPasswort',
+        EMAIL: 'email@email.com',
+        VORNAME: 'Max',
+        NACHNAME: 'Mustermann',
+        GEBURTSTAG: '2002-1-12',
+        INSTITUTION: 'NULL',
+        TELEFONNUMMER: 68468481446,
+        PLZ: 50667,
+        WOHNORT: 'Musterstraße 1',
+        GESCHLECHT: 'Männlich',
+      }
+    if(await api.isUsernameValid(user["USERNAME"])){
+        await api.createNewUser(user);
+    }
+}
+
+//Returns the profile data as Object
+//Access like profileData["VORNAME"]
+async function profileData(userId: number) {
+    var profileData:any = await api.getProfileData(userId);
+    console.log(profileData);
+}
+
+//Returns true if the username is valid
+async function validUsername(username:string) {
+    var isValid:boolean = await api.isUsernameValid(username);
+}

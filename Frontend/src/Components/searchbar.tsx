860fe0dcd9af86352a6296aef4137e7b2b405a31
Flamtky <n.schumann@rz-online.de>
2021-12-04 23:41:47 +0100

    added placeholder parameter for searchbar

--- Frontend/src/Components/searchbar.tsx
+++ Frontend/src/Components/searchbar.tsx
@@ -3,15 +3,15 @@ import { StyleSheet, useWindowDimensions } from "react-native";
 import { View, TextInput } from "react-native";
 import { BLACK, GRAY, LIGHTGRAY } from '../Constants/colors';
 
-export default function SearchBar() {
+export default function SearchBar(props: any) {
     const dimensions = useWindowDimensions();
     const isLargeScreen = dimensions.width >= 768;
     const [isFocused, setIsFocused] = useState(false);
     return (
-        <View style={[styles.container, !isLargeScreen && isFocused ? { width: "100%", height: "60%" } : null]}>
+        <View style={[styles.container, !isLargeScreen && isFocused ? props.mobileStyle : {}, props.style]}>
             <TextInput
                 style={styles.input}
-                placeholder={"Suchen..."}
+                placeholder={props.placeholder || "Suchen..."}
                 placeholderTextColor={GRAY}
                 returnKeyType="search"
                 onBlur={() => setIsFocused(false)}

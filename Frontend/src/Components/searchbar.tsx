c5e7d0186bc4d240aa5ee92377950835bd524292
Flamtky <n.schumann@rz-online.de>
2021-12-06 00:16:37 +0100

    added value to searchbar

--- Frontend/src/Components/searchbar.tsx
+++ Frontend/src/Components/searchbar.tsx
@@ -18,6 +18,7 @@ export default function SearchBar(props: any) {
                 onFocus={() => setIsFocused(true)}
                 spellCheck={false}
                 onChange={props.onChange}
+                value={props.value}
             />
         </View>
     );

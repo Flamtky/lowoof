bb0a52299ffd09f285c90e422f7b45b2b5894e70
Flamtky <n.schumann@rz-online.de>
2021-12-08 00:24:11 +0100

    change searchbar (mobile more left)

--- Frontend/src/Components/sidebar.tsx
+++ Frontend/src/Components/sidebar.tsx
@@ -36,7 +36,7 @@ export default function Sidebar() {
                     <SearchBar mobileStyle={{ width: "100%", height: "60%" }} />
                 ),
                 headerRightContainerStyle: {
-                    right: isLargeScreen ? "40%" : "0%",
+                    right: isLargeScreen ? "40%" : "10%",
                 },
                 headerStyle: {
                     backgroundColor: BACKGROUNDCOLOR,
@@ -56,7 +56,7 @@ export default function Sidebar() {
             }}
             backBehavior='history'
         >
-            <Drawer.Screen name="Mein Profil" component={Profile} initialParams={{ userID: 1 /* TODO: Give current UserID */ }} />
+            <Drawer.Screen name="Mein Profil" component={Profile} initialParams={{ userID: 2 /* TODO: Give current UserID */ }} />
             <Drawer.Screen name="Tierprofil" component={Tierprofil} />
             <Drawer.Screen name="Matches" component={Matches} />
             <Drawer.Screen name="Freunde" component={Freunde} />

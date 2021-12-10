fe381bbd1608a58880c0d597b28408e166e8b859
Flamtky <n.schumann@rz-online.de>
2021-12-10 08:20:35 +0100

    added first preparations for chatting

--- Frontend/src/Screens/Chat.tsx
+++ Frontend/src/Screens/Chat.tsx
@@ -6,17 +6,38 @@ import SearchBar from '../Components/searchbar';
 import { BACKGROUNDCOLOR, MAINCOLOR } from '../Constants/colors';
 import language from '../../language.json';
 import { currentLanguage } from '../Constants/language';
+import { Pet } from '../Api/interfaces';
+import { TextBlock } from '../Components/styledText';
 
 export function Chat(props: any) {
     const dimensions = useWindowDimensions();
     const isLargeScreen = dimensions.width >= 768;
     const [message, setMessage] = useState('');
+    const [messageHistory, setMessageHistory] = useState<string[]>([]);
+    const [targetPet, setTargetPet] = useState<Pet>(props.petID);
+
+    //TODO: Add a function to get the message history from the server
+    //TODO: Test displaying the message history
+    //TODO: Add a function to send a message to the server
+
+    React.useEffect(() => {
+        if (targetPet == undefined) {
+            props.navigation.navigate('MyProfile');
+        }
+        props.navigation.setParams({ name: "Chat with " + targetPet.NAME })
+    }, [targetPet]);
 
     return (
         <View style={{ backgroundColor: BACKGROUNDCOLOR, height: "100%" }}>
             <View style={[styles.container, isLargeScreen ? { width: '43%', left: "28%" } : null]}>
                 <ScrollView keyboardDismissMode='on-drag' style={{ height: "100%", width: "100%" }}>
-                    {/*Messages*/}
+                    {messageHistory.map((message, index) => {
+                        return (
+                            <View key={index} style={styles.messageContainer}>
+                                <TextBlock style={styles.messageText}>{message}</TextBlock>
+                            </View>
+                        )
+                    })}
                 </ScrollView>
                 <View style={{ width: "100%", flexDirection: 'row' }}>
                     <SearchBar
@@ -58,4 +79,17 @@ const styles = StyleSheet.create({
         flexGrow: 1,
         borderRadius: 0,
     },
+    messageContainer: {
+        width: "100%",
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: "#fff",
+        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    messageText: {
+        fontSize: 16,
+        color: "#000",
+    }
 });

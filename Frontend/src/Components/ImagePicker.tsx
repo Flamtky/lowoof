5f4847880330db87080a99abb95d35cc5c4c4161
lXetnosl <manuel_fridolin.wiebe@fh-bielefeld.de>
2021-12-11 16:31:59 +0100

    Updated language

--- Frontend/src/Components/ImagePicker.tsx
+++ Frontend/src/Components/ImagePicker.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import language from '../../language.json';
+import { currentLanguage } from '../Constants/language';
 
 export default function ImagePickerField(props:any) {
     const [image, setImage] = useState("");
@@ -11,7 +13,7 @@ export default function ImagePickerField(props:any) {
             if (Platform.OS !== 'web') {
                 const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
                 if (status !== 'granted') {
-                    alert('Sorry, we need camera roll permissions to make this work!');
+                    alert(language.IMAGE_PICKER.NEED_PERM[currentLanguage]);
                 }
             }
         })();
@@ -30,7 +32,7 @@ export default function ImagePickerField(props:any) {
 
     return (
         <View style={{alignItems: 'center', justifyContent: 'center' }}>
-            <Button title={props.title ?? "Pick an Image"} onPress={pickImage} color={"#0f70e6"}/>
+            <Button title={props.title ?? language.IMAGE_PICKER.PICK_IMAGE[currentLanguage]} onPress={pickImage} color={"#0f70e6"}/>
             {image && (props.showPreview ?? true) ? <Image source={{ uri: image }} style={{ width: 32, height: 32 }} /> : <></>}
         </View>
     );

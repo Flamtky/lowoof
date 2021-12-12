bdb241e66345512ca5920c1dcbfada99e3dfb6c1
Flamtky <n.schumann@rz-online.de>
2021-12-12 01:51:24 +0100

    added last message (not tested,api broken)

--- Frontend/src/Screens/Chats.tsx
+++ Frontend/src/Screens/Chats.tsx
@@ -8,7 +8,7 @@ import Seperator from '../Components/seperator';
 import OwnButton from '../Components/ownButton';
 import language from '../../language.json';
 import { currentLanguage } from '../Constants/language';
-import { Pet } from '../Api/interfaces';
+import { Message, Pet } from '../Api/interfaces';
 import { API } from '../Constants/api';
 import { Buffer } from 'buffer';
 
@@ -16,12 +16,20 @@ export default function Chats({ route, navigation }: any) {
     const dimensions = useWindowDimensions();
     const isLargeScreen = dimensions.width >= 768;
     const [pets, setPets] = React.useState<Pet[]>([]);
+    const [lastMessages, setLastMessages] = React.useState<string[]>([]);
     const [isLoading, setIsLoading] = React.useState(true);
     React.useEffect(() => {
         API.getChats(route.params?.pet?.TIERID).then((res: any) => {
             if (!res.hasOwnProperty("message")) {
                 setPets(res as Pet[]);
-                setIsLoading(false);
+                (res as Pet[]).forEach((pet: Pet) => {
+                    API.getLastMessage(route.params?.pet?.TIERID, pet.TIERID).then((res: any) => {
+                        if (!res.hasOwnProperty("message")) {
+                            setLastMessages([...lastMessages, (res as Message).NACHRICHT]);
+                            setIsLoading(false);
+                        }
+                    });
+                });
             } else if ((res as Response).status === 404) {
                 setIsLoading(false);
             } else if ((res as Response).status === 403) {
@@ -49,6 +57,7 @@ export default function Chats({ route, navigation }: any) {
                             pet={pet}
                             navigation={navigation}
                             ownID={route.params?.pet?.TIERID}
+                            lastMessage={lastMessages[index]/* TODO: TEST THIS */}
                         />
                     );
                 })}
@@ -64,9 +73,9 @@ export function ChatItem(props: any) {
     const pet: Pet = props.pet; /* The pet to display */
     return (
         <>
-            <TouchableOpacity style={[styles.row, { marginTop: 0, height: 60 }]} onPress={() => { props.navigation.navigate('Chat', {targetPet: pet.TIERID, ownPet: props.ownID, navigation: props.navigation}) }}>
+            <TouchableOpacity style={[styles.row, { marginTop: 0, height: 60 }]} onPress={() => { props.navigation.navigate('Chat', { targetPet: pet.TIERID, ownPet: props.ownID, navigation: props.navigation }) }}>
                 <View style={[styles.row, { width: "100%", marginLeft: 15, marginTop: 0 }]}>
-                    <TouchableOpacity onPress={() => { props.navigation.navigate('PetProfile', {petID: pet.TIERID})}}>
+                    <TouchableOpacity onPress={() => { props.navigation.navigate('PetProfile', { petID: pet.TIERID }) }}>
                         <Image style={styles.profilepicture}
                             source={{ uri: pet.PROFILBILD != null ? Buffer.from(pet.PROFILBILD, 'base64').toString('ascii') : "https://puu.sh/IsTPQ/5d69029437.png" }}
                         />
@@ -74,9 +83,9 @@ export function ChatItem(props: any) {
                     <View style={{ marginLeft: 10, height: 60, justifyContent: "space-between" }}>
                         <TextBlock>{language.PET.OWNER[currentLanguage]}: {pet.USERNAME}</TextBlock>
                         <TextBlock>{language.PET.NAME[currentLanguage]}: {pet.NAME}</TextBlock>
-                        <TextBlock>{language.CHATS.LAST_MSG[currentLanguage]}: </TextBlock>
+                        <TextBlock>{language.CHATS.LAST_MSG[currentLanguage]}: {props.lastMessage}</TextBlock>
                     </View>
-                    <TouchableOpacity onPress={() => { API.deleteChat(props.ownID, pet.TIERID) /* TODO: REALTIME UPDATE */}} style={{ marginLeft: "auto", alignSelf: "center", right: 20 }}>
+                    <TouchableOpacity onPress={() => { API.deleteChat(props.ownID, pet.TIERID) /* TODO: REALTIME UPDATE */ }} style={{ marginLeft: "auto", alignSelf: "center", right: 20 }}>
                         <FontAwesomeIcon icon={faTrashAlt} size={32} color="#555" />
                     </TouchableOpacity>
                 </View>
